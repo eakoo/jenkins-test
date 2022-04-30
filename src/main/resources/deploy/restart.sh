61b3094ad9d7a3ce6e49e4ed9818b6ad462c1272
apenda <apenda@yeah.net>
2022-04-30 16:54:42 +0800

    修改tips

--- src/main/resources/deploy/restart.sh
+++ src/main/resources/deploy/restart.sh
@@ -16,3 +16,11 @@ if [ "$CONTAINER_STATUS" == 0 ];
     sudo docker rm $APP_NAME
     sudo docker run -d -p $APP_PORT:$APP_PORT --name $APP_NAME $APP_NAME
 fi
+
+# 删除 NONE 镜像
+IMAGE_IDS=$(sudo docker images | grep'<none>' | awk '{print $3}')
+echo 'remove images start ..'
+echo IMAGE_IDS
+if [ -n "$IMAGE_IDS" ] ;then
+    sudo docker rmi "$IMAGE_IDS"
+fi
