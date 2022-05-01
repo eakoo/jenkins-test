2a0464c77975f1ef1b15ecc593a6cf584d0cf9e4
apenda <apenda@yeah.net>
2022-05-01 16:40:47 +0800

    修改Jenkins

--- src/main/java/com/eakoo/jenkins/controller/TestController.java
+++ src/main/java/com/eakoo/jenkins/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "This is a main branch";
+        return "This is a test branch";
     }
 
 }
